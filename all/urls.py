6b77b5d07b269b5124a9098c75b20df96bbd20a8
napolisiqueira <imfelipenapoli@gmail.com>
2025-07-04 19:27:38 -0300

    add new urls on app

--- all/urls.py
+++ all/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "all"
 urlpatterns = [
     path('', views.home, name="home"),
+    path('projects/<str:owner>/<str:repo_name>/doc/', views.project_documentation_view, name='project_doc'),
 ]
